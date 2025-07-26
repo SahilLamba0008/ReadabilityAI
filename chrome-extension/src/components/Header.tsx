3f5deeb8e8f00113484b59add74a9797f9a3559e
SahilLamba@2001 <19bcs1922@gmail.com>
2025-07-26 20:47:29 +0530

    moving ahead with CRXjs

--- chrome-extension/src/components/Header.tsx
+++ chrome-extension/src/components/Header.tsx
@@ -1,25 +1,19 @@
 import {
 	BookOpen,
 	Sun,
+	User,
 	// User
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
+
+import { dropdownMenuItems } from "@/lib/constants";
 import {
 	DropdownMenu,
 	DropdownMenuContent,
-	DropdownMenuGroup,
 	DropdownMenuItem,
-	DropdownMenuLabel,
-	DropdownMenuPortal,
 	DropdownMenuSeparator,
-	DropdownMenuShortcut,
-	DropdownMenuSub,
-	DropdownMenuSubContent,
-	DropdownMenuSubTrigger,
 	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-
-// import { dropdownMenuItems } from "@/lib/constants";
 
 const Header = () => {
 	return (
@@ -44,8 +38,8 @@ const Header = () => {
 					<Sun className="w-4 h-4" />
 				</Button>
 
-				{/* <DropdownMenu>
-					<DropdownMenuTrigger asChild>
+				<DropdownMenu>
+					<DropdownMenuTrigger>
 						<Button
 							variant="ghost"
 							size="sm"
@@ -70,66 +64,6 @@ const Header = () => {
 								</DropdownMenuItem>
 							);
 						})}
-					</DropdownMenuContent>
-				</DropdownMenu> */}
-
-				<DropdownMenu>
-					<DropdownMenuTrigger asChild>
-						<Button
-							variant="outline"
-							onClick={() => console.log("dropdown clicked")}
-						>
-							Open
-						</Button>
-					</DropdownMenuTrigger>
-					<DropdownMenuContent className="w-56" align="start">
-						<DropdownMenuLabel>My Account</DropdownMenuLabel>
-						<DropdownMenuGroup>
-							<DropdownMenuItem>
-								Profile
-								<DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-							</DropdownMenuItem>
-							<DropdownMenuItem>
-								Billing
-								<DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
-							</DropdownMenuItem>
-							<DropdownMenuItem>
-								Settings
-								<DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-							</DropdownMenuItem>
-							<DropdownMenuItem>
-								Keyboard shortcuts
-								<DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
-							</DropdownMenuItem>
-						</DropdownMenuGroup>
-						<DropdownMenuSeparator />
-						<DropdownMenuGroup>
-							<DropdownMenuItem>Team</DropdownMenuItem>
-							<DropdownMenuSub>
-								<DropdownMenuSubTrigger>Invite users</DropdownMenuSubTrigger>
-								<DropdownMenuPortal>
-									<DropdownMenuSubContent>
-										<DropdownMenuItem>Email</DropdownMenuItem>
-										<DropdownMenuItem>Message</DropdownMenuItem>
-										<DropdownMenuSeparator />
-										<DropdownMenuItem>More...</DropdownMenuItem>
-									</DropdownMenuSubContent>
-								</DropdownMenuPortal>
-							</DropdownMenuSub>
-							<DropdownMenuItem>
-								New Team
-								<DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
-							</DropdownMenuItem>
-						</DropdownMenuGroup>
-						<DropdownMenuSeparator />
-						<DropdownMenuItem>GitHub</DropdownMenuItem>
-						<DropdownMenuItem>Support</DropdownMenuItem>
-						<DropdownMenuItem disabled>API</DropdownMenuItem>
-						<DropdownMenuSeparator />
-						<DropdownMenuItem>
-							Log out
-							<DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
-						</DropdownMenuItem>
 					</DropdownMenuContent>
 				</DropdownMenu>
 			</div>
